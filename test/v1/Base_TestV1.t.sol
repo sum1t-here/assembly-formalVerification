3b5b0daf1441421cbb6015580d6a1ee1963e9ec5
sumit <mazumdarsumit3@gmail.com>
2025-05-27 18:10:49 +0530

    testing in foundry for huff

---
+++ test/v1/Base_TestV1.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {HorseStore} from "../../src/horseStoreV1/HorseStore.sol";
+import {Test, console2} from "forge-std/Test.sol";
+
+abstract contract Base_TestV1 is Test {
+    HorseStore public horseStore;
+
+    function setUp() public virtual {
+        horseStore = new HorseStore();
+    }
+
+    function testReadValue() public {
+        uint256 initalValue = horseStore.readNumberOfHorses();
+        assertEq(initalValue, 0);
+    }
+}
