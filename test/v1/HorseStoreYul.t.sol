da1a43654a2f58260a9b29b37db8ced302df809e
sumit <mazumdarsumit3@gmail.com>
2025-05-31 20:54:26 +0530

    horse store v2

--- test/v1/HorseStoreYul.t.sol
+++ test/v1/HorseStoreYul.t.sol
@@ -5,7 +5,7 @@ import {Base_TestV1, IHorseStore} from "./Base_TestV1.t.sol";
 import {HorseStoreYul} from "../../src/horseStoreV1/HorseStoreYul.sol";
 
 contract HorseStoreSolc is Base_TestV1 {
-    function setUp() public override{
+    function setUp() public override {
         horseStore = IHorseStore(address(new HorseStoreYul()));
     }
 }
